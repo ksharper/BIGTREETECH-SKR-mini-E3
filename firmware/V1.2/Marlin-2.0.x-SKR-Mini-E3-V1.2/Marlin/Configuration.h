4521881b19981cdd36ba27f1188bfc6b54923755
Kyle Harper <kylesharper@gmail.com>
2020-05-19 16:49:36 -0500

    Lowering default z offset to -2.6

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -946,7 +946,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET {-48,-8, -2.80 }
+#define NOZZLE_TO_PROBE_OFFSET {-48,-8, -2.60 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
